8611aecffb58f060f0e7b355678d2fa12e33d666
MLCampbell <marcus.l.campbell@gmail.com>
2020-07-21 22:04:56 +1200

    added fine css styling and started JavaScript code

---
+++ app.js
@@ -0,0 +1,27 @@
+/********************************* 
+Game rules:
+
+- The game has 2 players, playing in rounds
+- In each turn a player may roll a dice as many times as they wish.
+- Each result of the dice roll is added to their round score.
+- A player may choose to bank their score, in which case, their score is saved and the active player changes
+- If a player rolls a '1', all of their round score is lost.active
+- First player to a banked total of 100 wins.
+*/
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<u>' + dice + '</u>'
+
+var x = document.querySelector('#score-' + activePlayer).textContent;
+console.log(x);
+
+document.querySelector('.dice-img').style.visibility = 'hidden';
