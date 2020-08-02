a541c9aaf126bf036837ac4dee0a133f892deb2e
MLCampbell <marcus.l.campbell@gmail.com>
2020-08-02 22:43:28 +1200

    Updated js to enable player switching

--- app.js
+++ app.js
@@ -9,21 +9,49 @@ Game rules:
 - First player to a banked total of 100 wins.
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
-var x = document.querySelector('#score-' + activePlayer).textContent;
-console.log(x);
 
 document.querySelector('.dice-img').style.visibility = 'hidden';
 
-document.querySelector('#panel-' + activePlayer).classList.add('active');
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    
+    //1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice-img')
+    document.querySelector('.dice-img').style.visibility = 'visible';
+    diceDOM.src = './img/kismet-' + dice + '.png';
+
+    // 3. Update round score IF the rolled number is not a 1
+
+    if (dice !== 1) {
+        roundScore += dice
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        document.querySelector('.switch-players').textContent = null;
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.switch-players').textContent = 'Score lost!'
+    }
+        
+});
 
